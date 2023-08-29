68283e101a6c64515de79f1e4394a50961c06c81
saitemre.yukselen1 <saitemre.yukselen1@nhs.net>
2023-08-29 23:17:02 +0100

    added some tests

--- tests/test_epytree.py
+++ tests/test_epytree.py
@@ -1,4 +1,22 @@
-from epytree import Tree
+from epytree import Tree, Node
+
 
 def test_add_node():
-    t
+    t = Tree()
+    n1 = Node()
+    n1.name = "note 1"
+    n1.data = "data 1"
+    t.add_node(n1, 0)
+    assert t.get_node(1).name == "note 1"
+
+# def test_del_node():
+#     assert False
+
+
+def test_get_node():
+    t = Tree()
+    n1 = Node()
+    n1.name = "note 1"
+    n1.data = "data 1"
+    t.add_node(n1, 0)
+    assert t.get_node(1).name == "note 1"
