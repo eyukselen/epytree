9bea76cdc7ab494ede3f8412d9939f95dbdacaf3
saitemre.yukselen1 <saitemre.yukselen1@nhs.net>
2023-09-01 23:56:26 +0100

    added json load

--- epytree.py
+++ epytree.py
@@ -52,3 +52,32 @@ class Tree:
 
     def to_json(self):
         return json.dumps(self.root, indent=2, cls=NodeEncoder)
+
+    def save(self, filename):
+        with open(filename, 'w') as f:
+            f.write(self.to_json())
+
+    def load(self, filename):
+        self.root = Node(name='Root', idx=0)
+        self.max_id = 0
+        self.map = {0: self.root}
+        with open(filename, 'r') as f:
+            json_tree = json.loads(f.read())
+        self.loads(json_tree)
+
+    def loads(self, js, level=0):
+        for k, v in js.items():
+            if k == "children" and len(v) > 0:
+                print(' ' * level, k)
+                for kk, vv in v.items():
+                    level += 1
+                    self.loads(vv, level)
+            else:
+                print(' ' * level, k, v)
+
+
+t = Tree()
+t.load('tree.json')
+
+
+# {'id': 0, 'parent_id': None, 'name': 'Root', 'data': None, 'children': {'1': {'id': 1, 'parent_id': 0, 'name': 'note 1', 'data': None, 'children': {'2': {'id': 2, 'parent_id': 1, 'name': 'note 1-1', 'data': None, 'children': {}}}}, '3': {'id': 3, 'parent_id': 0, 'name': 'note 2', 'data': None, 'children': {'4': {'id': 4, 'parent_id': 3, 'name': 'note 1-1-1', 'data': None, 'children': {}}}}}}
